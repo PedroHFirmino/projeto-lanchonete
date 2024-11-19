57fd82829e5d56bb56461bde8fe8876ecae33c0d
Pedro Firmino <pedrohefirmino@gmail.com>
2024-11-19 15:59:51 -0300

    Start

---
+++ src/test/java/pedro/firmino/lanches/LanchesApplicationTests.java
@@ -0,0 +1,13 @@
+package pedro.firmino.lanches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
